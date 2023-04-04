362c81a44eb9ed7641ff0611223ee1aa72f41a7d
Simon Siva <simonsiva13@gmail.com>
2023-04-04 18:05:43 +0300

    fix seeder validation

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -22,13 +22,16 @@ class UserSeeder extends Seeder
             'last_name' => 'Siva',
             'is_admin' => 1,
             'address' => 'PO BOX 254-00100',
-            'phone_number' => '0707898345',
+            'phone_number' => '254717898345',
             'is_marketing' => 0,
             'email' => 'admin@buckhill.co.uk',
-            'password' => Hash::make('admin'),
+            'password' => 'adminadmin',
+            'password_confirmation' => 'adminadmin',
         ];
         $validator = Validator::make($adminUser, $userRequest->rules());
         if ($validator->passes()) {
+            unset($adminUser['password_confirmation']);
+            $adminUser['password'] = Hash::make('admin');
             User::create($adminUser);
         }
 
@@ -37,17 +40,20 @@ class UserSeeder extends Seeder
             'last_name' => 'Mulwa',
             'is_admin' => 0,
             'address' => 'PO BOX 254-00100',
-            'phone_number' => '0707898345',
+            'phone_number' => '254707898345',
             'is_marketing' => 1,
             'email' => 'marketing@buckhill.co.uk',
-            'password' => Hash::make('marketing'),
+            'password' => 'marketingmarketing',
+            'password_confirmation' => 'marketingmarketing',
         ];
         $validator = Validator::make($marketingUser, $userRequest->rules());
         if ($validator->passes()) {
+            unset($marketingUser['password_confirmation']);
+            $marketingUser['password'] = Hash::make('marketing');
             User::create($marketingUser);
         }
 
-
+        User::factory(5)->create();
 
     }
 }
