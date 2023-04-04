e00fcfa72d8e9913038e12c80eb7b839da8a13b7
Simon Siva <simonsiva13@gmail.com>
2023-04-04 21:42:43 +0300

    create admin controller

---
+++ app/Http/Controllers/Api/Auth/AdminController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Date 04/04/2023
+ *
+ * @author   Simon Siva <simonsiva13@gmail.com>
+ */
+
+namespace App\Http\Controllers\Api\Auth;
+
+use App\Http\Controllers\Api\ApiController;
+
+class AdminController extends ApiController
+{
+
+}
