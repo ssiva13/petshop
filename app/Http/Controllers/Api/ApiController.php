27d27cc5ead56809988cd13e1a70b47e542b22f8
Simon Siva <simonsiva13@gmail.com>
2023-04-05 09:31:49 +0300

    create common api response handlers

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -11,15 +11,30 @@ use App\Handlers\AuthHandler;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\JsonResponse;
 use Lcobucci\JWT\UnencryptedToken;
+use Symfony\Component\HttpFoundation\Response as HttpResponse;
 
 class ApiController extends Controller
 {
-    public function sendResponse($data, $message, $status = 200): JsonResponse
+    public function sendSuccessResponse($data, $status = HttpResponse::HTTP_OK, $errors = [], $extra = []): JsonResponse
     {
         $response = [
             'success' => true,
             'data' => $data,
-            'message' => $message,
+            'error' => null,
+            'errors' => $errors,
+            'extra' => $extra,
+        ];
+        return response()->json($response, $status);
+    }
+
+    public function sendErrorResponse($message, $status = HttpResponse::HTTP_UNAUTHORIZED, $trace = []): JsonResponse
+    {
+        $response = [
+            'success' => false,
+            'data' => [],
+            'error' => $message,
+            'errors' => [],
+            'trace' => $trace,
         ];
         return response()->json($response, $status);
     }
@@ -30,14 +45,23 @@ class ApiController extends Controller
         return $authHandler->generateToken($user);
     }
 
-    protected function sendError($message, $errorData = [], $status = 400): JsonResponse
+    /**
+     * @param $message
+     * @param array $errorTrace
+     * @param int $status
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    protected function throwError($message, array $errorTrace = [], int $status = HttpResponse::HTTP_INTERNAL_SERVER_ERROR): JsonResponse
     {
         $response = [
-            'message' => $message,
+            'success' => false,
+            'data' => [],
+            'error' => $message,
+            'errors' => [],
+            'trace' => $errorTrace,
         ];
-        if (!empty($errorData)) {
-            $response['data'] = $errorData;
-        }
+
         return response()->json($response, $status);
     }
 }
