02a5dda54837a101eeb619a5399c2a07d560edb8
Simon Siva <simonsiva13@gmail.com>
2023-04-06 01:13:51 +0300

    create category model and seeder

---
+++ database/factories/CategoryFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Category>
+ */
+class CategoryFactory extends Factory
+{
+    protected string $title;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     * @throws \Exception
+     */
+    public function definition(): array
+    {
+        $this->title = fake()->text(random_int(5,10));
+        return [
+            'title' => fake()->firstName(),
+            'slug' => Str::slug($this->title),
+        ];
+    }
+}
