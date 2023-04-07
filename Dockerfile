3d67aa1cb73163ad2c53be2feed8ecda4fec125c
Simon Siva <simonsiva13@gmail.com>
2023-04-08 02:15:43 +0300

    add npm to docker

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,8 @@
 FROM php:8.2-fpm
 
+# Add NodeSource Node.js Binary Distributions
+RUN curl -fsSL https://deb.nodesource.com/setup_18.x | bash -
+
 # Install dependencies
 RUN apt-get update && apt-get install -y \
         nginx \
@@ -9,6 +12,8 @@ RUN apt-get update && apt-get install -y \
         libonig-dev \
         libxml2-dev \
         curl \
+        nodejs \
+        build-essential \
         wget \
         vim \
         libzip-dev \
@@ -29,6 +34,9 @@ RUN docker-php-ext-install \
         && pecl install xdebug \
         && docker-php-ext-enable xdebug
 
+# Clear cache
+RUN apt-get clean && rm -rf /var/lib/apt/lists/*
+
 # Install Composer
 RUN curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
 
@@ -43,6 +51,12 @@ COPY . .
 
 # Install composer dependencies
 RUN composer install --no-interaction --prefer-dist
+
+# Install npm dependencies
+RUN npm install
+
+# Build npm dependencies
+RUN npm run build
 
 # Expose port 80
 EXPOSE 80
--- docker/setup.sh
+++ docker/setup.sh
@@ -29,10 +29,10 @@ echo -e "\n${BLUE} Please wait while installing Composer packages ... \n ${NC}" 
     && composer install --no-interaction --prefer-dist \
     && echo -e "\n${GREEN} Composer Packages Installed ... \n ${NC}"
 
-#echo -e "\n${BLUE} Please wait while installing Node PacÏkages ... \n ${NC}" \
-#    && npm install \
-#    && echo -e "\n${GREEN} Node Packages Installed ... \n ${NC}"
-#
+echo -e "\n${BLUE} Please wait while installing Node PacÏkages ... \n ${NC}" \
+    && npm install \
+    && echo -e "\n${GREEN} Node Packages Installed ... \n ${NC}"
+
 
 # Run `npm rebuild node-sass` to download the binding for your current environment
 #echo -e "\n${BLUE} Please wait while building Node Sass Packages ... \n ${NC}" \
@@ -40,9 +40,9 @@ echo -e "\n${BLUE} Please wait while installing Composer packages ... \n ${NC}" 
 #    && echo -e "\n${GREEN} Node Packages built ... \n ${NC}"
 #
 
-#echo -e "\n${BLUE} Please wait while building Node Packages ... \n ${NC}" \
-#    && npm run dev \
-#    && echo -e "\n${GREEN} Node Packages built ... \n ${NC}"
+echo -e "\n${BLUE} Please wait while building Node Packages ... \n ${NC}" \
+    && npm run build \
+    && echo -e "\n${GREEN} Node Packages built ... \n ${NC}"
 
 
 php artisan key:generate --show
