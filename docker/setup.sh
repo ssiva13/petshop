8b15162b984775474ba17ec2d4d1c0406b891e68
Simon Siva <simonsiva13@gmail.com>
2023-04-12 01:21:47 +0300

    ffff

--- docker/setup.sh
+++ docker/setup.sh
@@ -25,18 +25,18 @@ touch storage/logs/laravel.log \
 
 
 # Install dependencies
-echo -e "\n${BLUE} Please wait while installing Composer packages ... \n ${NC}" \
-    && composer install --no-interaction --prefer-dist \
-    && echo -e "\n${GREEN} Composer Packages Installed ... \n ${NC}"
+# echo -e "\n${BLUE} Please wait while installing Composer packages ... \n ${NC}" \
+#     && composer install --no-interaction --prefer-dist \
+#     && echo -e "\n${GREEN} Composer Packages Installed ... \n ${NC}"
 
-echo -e "\n${BLUE} Please wait while installing Node PacÏkages ... \n ${NC}" \
-    && npm install \
-    && echo -e "\n${GREEN} Node Packages Installed ... \n ${NC}"
+# echo -e "\n${BLUE} Please wait while installing Node PacÏkages ... \n ${NC}" \
+#     && npm install \
+#     && echo -e "\n${GREEN} Node Packages Installed ... \n ${NC}"
 
 
-echo -e "\n${BLUE} Please wait while building Node Packages ... \n ${NC}" \
-    && npm run build \
-    && echo -e "\n${GREEN} Node Packages built ... \n ${NC}"
+# echo -e "\n${BLUE} Please wait while building Node Packages ... \n ${NC}" \
+#     && npm run build \
+#     && echo -e "\n${GREEN} Node Packages built ... \n ${NC}"
 
 
 php artisan migrate --seed
